72154339c95e344a0c6c230c37162668f1a45855
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2026-05-13 15:08:06 -0500

    Avance 1.0

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,9 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name: 'Insumos, textiles y maquinaria de confección',
+    Description:
+      'Mediante el estudio de este componente formativo el aprendiz logrará identificar y reconocer los insumos requeridos en cada una de las líneas de producto, las propiedades de los textiles y su composición para diferenciar su uso en las diferentes líneas de producción. Clasificar la maquinaria de confección, analizar los mecanismos de funcionamiento de la maquinaria para una óptima puesta a punto y proceder a su manejo, además, describe los ejercicios básicos para el manejo de las máquinas básicas. Reconocer las normas de calidad y la estructura de la norma internacional ISO 9001:2015.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
@@ -31,13 +32,20 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Materiales e insumos de confección',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo:
+              'Clasificación y descripción de materiales de acuerdo con procesos de producción',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo:
+              'Clasificación y descripción de insumos de acuerdo con líneas de producción',
+            hash: 't_1_2',
           },
         ],
       },
@@ -45,14 +53,115 @@ export default {
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Textiles',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Clasificación y descripción de textiles',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Clasificación y propiedades de hilos',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: ' Clasificación y usos de tejidos',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo:
+              'Aplicación de textiles de acuerdo con líneas de producción',
+            hash: 't_2_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Maquinaria de confección industrial',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Enhebrados de maquinaria de confección industrial',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2.1',
+            titulo: 'Maquinaria básica',
+            hash: 't_3_2_1',
+          },
+          {
+            numero: '3.2.2',
+            titulo: 'Maquinaria especializada',
+            hash: 't_3_2_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Nivel de arrastre: ajuste de maquinaria',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.3.1',
+            titulo: 'Tipos de ajustes de maquinaria según línea de producto',
+            hash: 't_3_3_1',
+          },
+          {
+            numero: '3.4',
+            titulo: 'Calibración de puntada',
+            hash: 't_3_4',
+          },
+          {
+            numero: '3.4.1',
+            titulo: 'Maquinaria básica',
+            hash: 't_3_4_1',
+          },
+          {
+            numero: '3.5',
+            titulo:
+              'Puesta a punto de la maquinaría y prueba de costura según operación de confección',
+            hash: 't_3_5',
+          },
+          {
+            numero: '3.6',
+            titulo:
+              'Manejo y operaciones básicas de máquinas de confección industrial',
+            hash: 't_3_6',
+          },
+        ],
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Normas de calidad',
+        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo: 'Normas técnicas colombianas en confección',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.1.1',
+            titulo: 'Normas técnicas en textiles',
+            hash: 't_4_1_1',
+          },
+          {
+            numero: '4.1.2',
+            titulo: 'Normas técnicas en confección',
+            hash: 't_4_1_2',
+          },
+          {
+            numero: '4.2',
+            titulo:
+              'Normas de calidad internacional: ISO 9001 Versión 2015. Sistema de Gestión de la Calidad – Requisitos',
+            hash: 't_4_2',
+          },
+        ],
       },
     ],
     subMenu: [
--- src/config/titulo.js
+++ src/config/titulo.js
@@ -1,1 +1,1 @@
-module.exports = 'Ecored Base PKG'
+module.exports = 'Insumos, textiles y maquinaria de confección'
